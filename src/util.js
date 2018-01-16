8ebedf6765c57a08cf6e247e9cafc51f08193ef0
Nick Pegg <nick@nickpegg.com>
2018-01-15 20:30:27 -0800

    get rid of JS garbage

--- src/util.js
+++
@@ -1,9 +0,0 @@
-import baseSlugify from 'slugify';
-
-
-function slugify(s) {
-  // Slugify a string to our specs. Hooray consistency!
-  return baseSlugify(s).toLowerCase();
-}
-
-export { slugify };
